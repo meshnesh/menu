4b4c899c5b81a191637ff49caa3d18738dfac918
meshnesh <tonnie.nesh@gmail.com>
2019-08-03 15:51:21 +0300

    save data to firebase

--- app/src/main/java/com/main/menu/MainActivity.kt
+++ app/src/main/java/com/main/menu/MainActivity.kt
@@ -2,11 +2,49 @@ package com.main.menu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.core.view.isEmpty
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var editMenuName: EditText
+    lateinit var categories: Spinner
+    lateinit var buttonSave: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editMenuName = findViewById(R.id.editMenuName)
+        categories = findViewById(R.id.spinner)
+        buttonSave = findViewById(R.id.button)
+
+        buttonSave.setOnClickListener() {
+            saveMenu()
+        }
+
+    }
+
+    private fun saveMenu() {
+        val name = editMenuName.text.toString().trim()
+        val category = categories.selectedItem.toString().trim()
+
+        if (name.isEmpty()) {
+            editMenuName.error = "Please enter a name "
+            return
+        }
+
+
+        val ref = FirebaseDatabase.getInstance().getReference("menus")
+        val menuId = ref.push().key.toString()
+
+        val menu = Menu(menuId, name, category)
+
+        ref.child(menuId).setValue(menu).addOnCompleteListener{
+            Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show()
+        }
     }
 }
---
+++ app/src/main/java/com/main/menu/Menu.kt
@@ -0,0 +1,3 @@
+package com.main.menu
+
+class Menu(val id: String, val name: String, val category: String)
